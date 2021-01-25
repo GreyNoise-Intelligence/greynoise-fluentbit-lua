2d3b808a385432ffccba06763310f3bded1fc94f
Matt Lehman <matt@greynoise.io>
2021-01-25 00:29:35 -0500

    Fix broken tests and update makefile

--- Makefile
+++ Makefile
@@ -3,27 +3,29 @@
 CONTAINER := gn-fluentbit
 WORKDIR := /app
 
-.docker-build
+.docker-build: build
+	@touch .docker-build
+
+.PHONY: build
 build:
 	docker build -t $(CONTAINER) .
-	@touch .docker-build
 
 .PHONY: clean
 clean:
 	@rm -rf examples/output.log .docker-build
 
 .PHONY: shell
-shell:
+shell: .docker-build
 	docker run -it -v $(PWD):$(WORKDIR) $(CONTAINER) --shell
 
 .PHONY: run-tail
-run-tail: clean
+run-tail: clean .docker-build
 	docker run -e FB_CONF_FILE=examples/tail.conf -it -p 2020:2020 -v $(PWD):$(WORKDIR) $(CONTAINER)
 
 .PHONY: run
-run: clean
+run: clean .docker-build
 	docker run -e FB_CONF_FILE=examples/dummy.conf -it -p 2020:2020 -v $(PWD):$(WORKDIR) $(CONTAINER)
 
 .PHONY: test
-test: build
+test: .docker-build
 	docker run -it -v $(PWD):$(WORKDIR) $(CONTAINER) --test
--- lua/test.lua
+++ lua/test.lua
@@ -4,50 +4,50 @@ describe("check_ip handler", function()
     it("tests check_ip handler with valid IPv4", function()
         ip = '89.248.172.16'
         local record = {}
-        local v, _ = greynoise.check_ip(record, ip)
-        assert.True(v)
+        local v = greynoise.check_ip(record, ip)
+        assert.False(v["gn_invalid"])
     end)
     it("tests check_ip handler with private IPv4", function()
         ip = '192.168.100.1'
         local record = {}
-        local v, _ = greynoise.check_ip(record, ip)
-        assert.False(v)
+        local v = greynoise.check_ip(record, ip)
+        assert.True(v["gn_bogon"])
     end)
     it("tests check_ip handler with localhost IPv4", function()
         ip = '127.0.0.1'
         local record = {}
-        local v, _ = greynoise.check_ip(record, ip)
-        assert.False(v)
+        local v = greynoise.check_ip(record, ip)
+        assert.True(v["gn_bogon"])
     end)
     it("tests check_ip handler with multicast IPv4", function()
         ip = '224.0.2.1'
         local record = {}
-        local v, _ = greynoise.check_ip(record, ip)
-        assert.False(v)
+        local v = greynoise.check_ip(record, ip)
+        assert.True(v["gn_bogon"])
     end)
     it("tests check_ip handler with private IPv6", function()
         ip = '2001:0db8:85a3:0000:0000:8a2e:0370:7334'
         local record = {}
-        local v, _ = greynoise.check_ip(record, ip)
-        assert.False(v)
+        local v = greynoise.check_ip(record, ip)
+        assert.True(v["gn_invalid"])
     end)
     it("tests check_ip handler with valid IPv6", function()
         ip = '2a03:2880:f003:c07:face:b00c::2'
         local record = {}
-        local v, _ = greynoise.check_ip(record, ip)
-        assert.False(v)
+        local v = greynoise.check_ip(record, ip)
+        assert.True(v["gn_invalid"])
     end)
     it("tests check_ip handler with nil source_ip", function()
         ip = nil
         local record = {}
-        local v, _ = greynoise.check_ip(record, ip)
-        assert.False(v)
+        local v = greynoise.check_ip(record, ip)
+        assert.True(v["gn_invalid"])
     end)
     it("tests check_ip handler with non IPv4/IPv6 source_ip", function()
         ip = '1.2.3.test_junk_source_ip'
         local record = {}
-        local v, _ = greynoise.check_ip(record, ip)
-        assert.False(v)
+        local v = greynoise.check_ip(record, ip)
+        assert.True(v["gn_invalid"])
     end)
   end)
 
