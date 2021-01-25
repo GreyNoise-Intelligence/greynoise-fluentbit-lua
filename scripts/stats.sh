df49e72b8696fd0d521ecae49154564998343967
Matt Lehman <matt@greynoise.io>
2021-01-25 01:43:04 -0500

    Add bogon/invalid status

--- scripts/stats.sh
+++ scripts/stats.sh
@@ -6,11 +6,17 @@ bogon=$(jq -c '. | select(.gn_bogon == true)' examples/output.log | wc -l)
 invalid=$(jq -c '. | select(.gn_invalid == true)' examples/output.log | wc -l)
 noise_percent=$(echo "scale=4; $quick/$total" | bc -l)
 riot_percent=$(echo "scale=4; $riot/$total" | bc -l)
+bogon_percent=$(echo "scale=4; $bogon/$total" | bc -l)
+invalid_percent=$(echo "scale=4; $invalid/$total" | bc -l)
 
 echo "--Line Counts--"
-echo "Total: $total"
-echo "Noise: $quick"
-echo "RIOT:  $riot"
+echo "Total:    $total"
+echo "Noise:    $quick"
+echo "RIOT:     $riot"
+echo "Bogon:    $bogon"
+echo "Invalid:  $invalid"
 echo "--Percentages--"
 echo "Noise:    $noise_percent"
 echo "RIOT:     $riot_percent"
+echo "Bogon:    $bogon_percent"
+echo "Invalid:  $invalid_percent"
